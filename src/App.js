fc0ed572c78a1257e480b2c01227590f49b156f2
Wardi <wardi@wardi-s.com>
2022-06-22 00:51:29 +0100

    fetch repos from github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
 import {
   ChakraProvider,
   Box,
@@ -14,10 +16,34 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   const [term, setTerm] = useState('');
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(term);
+
+    fetchRepos();
+  };
+
+  const fetchRepos = async () => {
+    setLoading(true);
+
+    const url = 'https://api.github.com/search/repositories';
+    await axios
+      .get(url, {
+        params: {
+          sort: 'stars',
+          per_page: 25,
+          q: term,
+        },
+      })
+      .then(res => {
+        const { data } = res;
+        if (data.items.length) setRepos(data.items);
+      })
+      .catch(err => console.log(err));
+
+    setLoading(false);
   };
 
   return (
@@ -30,12 +56,25 @@ function App() {
 
             <HStack as="form" onSubmit={handleSubmit}>
               <Input
-                placeholder="Search..."
+                placeholder="Search term..."
                 onChange={e => setTerm(e.target.value)}
               />
-              <Button type="submit">Search</Button>
+              <Button
+                type="submit"
+                isLoading={loading}
+                loadingText="Searching"
+                variant="outline"
+              >
+                Search
+              </Button>
             </HStack>
-            <p>{term}</p>
+
+            {repos &&
+              repos.map(repo => (
+                <p key={repo.id}>
+                  {repo.name} - {repo.stargazers_count}
+                </p>
+              ))}
           </VStack>
         </Grid>
       </Box>
