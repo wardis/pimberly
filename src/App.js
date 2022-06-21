1183855575b7af9163bd9fb92253ddacd0c64197
Wardi <wardi@wardi-s.com>
2022-06-22 00:24:09 +0100

    create search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { ChakraProvider, Box, Grid, theme, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Grid,
+  theme,
+  VStack,
+  Heading,
+  HStack,
+  Input,
+  Button,
+} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
+  const [term, setTerm] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(term);
+  };
+
   return (
     <ChakraProvider theme={theme}>
       <Box fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" pos="fixed" />
-          <Text>Github Repos Search</Text>
+          <VStack spacing={8} alignItems="center">
+            <Heading>GitHub Repos</Heading>
+
+            <HStack as="form" onSubmit={handleSubmit}>
+              <Input
+                placeholder="Search..."
+                onChange={e => setTerm(e.target.value)}
+              />
+              <Button type="submit">Search</Button>
+            </HStack>
+            <p>{term}</p>
+          </VStack>
         </Grid>
       </Box>
     </ChakraProvider>
